82193768f12473b164337fd18034a6cfe76b6d40
sunz8281 <sunz8281@gmail.com>
2025-07-09 05:06:32 +0900

    feat(#15): Header button 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,16 +1,21 @@
 import styled from '@emotion/styled';
 
 import { color } from '@tokens';
-import { Text } from '@components';
+import { Button, Text } from '@components';
 
 interface HeaderProps {
   title: string;
+  button?: {
+    text?: string;
+    leftIcon?: string;
+  };
 }
 
-const Header = ({ title }: HeaderProps) => {
+const Header = ({ title, button }: HeaderProps) => {
   return (
     <StyledHeader>
       <Text variant='T_SB_16'>{title}</Text>
+      {button && <Button type='outlined' size='small' {...button} />}
     </StyledHeader>
   );
 };
@@ -23,5 +28,6 @@ const StyledHeader = styled.div`
   height: 64px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   border-bottom: 1px solid ${color.grayscale['100']};
 `;
